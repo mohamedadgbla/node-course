d9946341355fe78817b97f746b151ac1f419c101
maomaster <maomaster2000@gmail.com>
2025-09-21 05:21:09 -0500

    added the add file

--- server.js
+++ server.js
@@ -1,2 +1,24 @@
  console.log('welcome to node series');
+
+//  console.log(global);
+
+ const os = require('os');
+ const path = require('path');
+//  console.log(os.type());
+//  console.log(os.homedir());
+//  console.log(os.version());
+
+//  console.log(__dirname);
+//  console.log(__filename);
+//  console.log(path.dirname(__filename));
+
+
  
+
+ const {add, sub} = require('./math');
+//  console.log(math.add(2,3));
+//  console.log(math.sub(44,23));
+
+ console.log(add(2,77));
+ console.log(sub(0,3));
+
